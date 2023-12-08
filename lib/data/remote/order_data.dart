82454e8393362fa168de99ab7474df7241a4ab59
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2023-12-08 22:50:01 +0300

    edit OrderRemoteData

--- lib/data/remote/order_data.dart
+++ lib/data/remote/order_data.dart
@@ -8,5 +8,26 @@ import '../../core/services/dependency_injection.dart';
 class OrderRemoteData {
   final _crud = AppInjection.getIt<Crud>();
 
+  Future<Either<ParentState, Map<String, dynamic>>> getOrders() async {
+    final token = AppLocalData.user!.authorization!;
+    final response = await _crud.getData(
+      // todo: url
+      linkUrl: AppLink.order,
+      token: token,
+    );
+    return response;
+  }
 
+  Future<Either<ParentState, Map<String, dynamic>>> getOrderDetails({
+    required Map<String, dynamic> queryParameters,
+  }) async {
+    final token = AppLocalData.user!.authorization!;
+    final response = await _crud.getData(
+      // todo: url
+      linkUrl: AppLink.order,
+      token: token,
+      queryParameters: queryParameters,
+    );
+    return response;
+  }
 }
